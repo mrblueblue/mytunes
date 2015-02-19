f564ed4076b7b649b80a572071b713c4feeb3c11
mrblueblue <grokut@gmail.com>
2015-02-18 17:36:27 -0800

    completed all but one spec test

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -6,11 +6,20 @@ var PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
+
+  },
+
+  events: {
+    'ended': function(){
+      this.model.ended();
+    }
   },
 
   setSong: function(song){
     this.model = song;
     this.render();
+
+
   },
 
   render: function(){
